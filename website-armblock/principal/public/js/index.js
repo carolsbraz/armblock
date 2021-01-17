13c5577f18227cffedf809fbe64d37e701fc8492
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2021-01-17 16:02:52 -0400

    update-responsividade

--- website-armblock/principal/public/js/index.js
+++ website-armblock/principal/public/js/index.js
@@ -6,7 +6,7 @@ animeScroll();
 function animeScroll() {
     const pageTop = window.pageYOffset;
     target.forEach(function(element) {
-        if ((pageTop) > element.offsetTop - ((window.innerHeight * 3) / 4)) {
+        if ((pageTop) > element.offsetTop - ((window.innerHeight * 3.5) / 4)) {
             element.classList.add(animationClass)
         }
     })
