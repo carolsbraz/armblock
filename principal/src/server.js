823f8b73bb60500ba46d0d9ab2b6e8e99e552d65
kluiwert <kluiwertvictor@super.ufa.edu.br>
2021-02-04 23:55:16 -0400

    update

--- principal/src/server.js
+++ principal/src/server.js
@@ -44,8 +44,8 @@ server.get("/trail-content-5", (req, res) => {
 server.get("/trail-content-6", (req, res) => {
     res.sendFile(__dirname + "/views/trail-content-6.html")
 })
-server.get("/trail-content-7", (req, res) => {
-    res.sendFile(__dirname + "/views/trail-content-7.html")
+server.get("/trail-content-10", (req, res) => {
+    res.sendFile(__dirname + "/views/trail-content-10.html")
 })
 server.get("/operational-programming", (req, res) => {
     var avaiableSerialPorts = []
