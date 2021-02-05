fb178693f86c32e609ba289844bbbc007a164724
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2021-02-05 00:10:38 -0400

    style: block programming

--- principal/public/js/block-programming.js
+++ principal/public/js/block-programming.js
@@ -1,6 +1,9 @@
-const board = document.getElementById('board')
-const dragboard = document.getElementById('drag-board')
+const board = document.getElementById('programming-blocks')
 
+let pontos = 1;
+
+
+const dragboard = document.getElementById('here-blocks')
 new Sortable(dragboard, {
     group: {
         name: 'shared',
@@ -9,31 +12,7 @@ new Sortable(dragboard, {
     },
     animation: 150,
     sort: false,
-    onEnd: function editavel(evt) {
-
-        //inputs
-
-        var itemEl = evt.item;
-        var input = itemEl.querySelectorAll('.board .block input')
-        var select = itemEl.querySelectorAll('.board .block select')
-
-        input.forEach(inp => {
-            console.log(inp)
-            inp.disabled = false
-        })
-        select.forEach(slc => {
-            console.log(slc)
-            slc.disabled = false
-        })
-
-        // contaniner
-
-        var containers = null;
-        containers = document.querySelectorAll(".drag");
-        for (var i = 0; i < containers.length; i++) {
-            new Sortable(containers[i], sortableOptions2);
-        }
-    }
+    filter: '.undrag'
 });
 
 new Sortable(board, {
@@ -41,25 +20,78 @@ new Sortable(board, {
     animation: 150
 });
 
-var sortableOptions2 = {
-    group: {
-        name: "sortable-list-2",
-        pull: true,
-        put: true,
-    },
-    animation: 2
-};
+const btnAdd = document.getElementById('add-button')
+const hereBlocks = document.getElementById('here-blocks')
 
-const hi = document.getElementById('hi')
+btnAdd.addEventListener('click', () => {
+    const newBoard = document.createElement('div')
+    newBoard.classList.add('block', 'yellow')
 
+    // yellow content
+    pontos++;
+    const idt = document.createElement('span')
+    var idttxt = document.createTextNode(`(Ponto ${pontos} ) Mover o motor`);
+    idt.appendChild(idttxt)
+    const motorInput = document.createElement('input')
+    motorInput.setAttribute('type', 'number')
+    motorInput.setAttribute('placeholder', 'motor')
 
+    // internal yellow
 
-hi.addEventListener('click', () => {
+    const drag = document.createElement('div')
+    drag.classList.add('drag')
+
+    // blue content
+
+    const blueBlock = document.createElement('div')
+    blueBlock.classList.add('block', 'blue')
+
+    var degre = document.createTextNode(`Para`);
+    blueBlock.appendChild(degre)
+
+    const degreInput = document.createElement('input')
+    degreInput.setAttribute('type', 'number')
+    degreInput.setAttribute('placeholder', 'graus')
+    blueBlock.appendChild(degreInput)
+
+    var degresymbol = document.createTextNode(`°`);
+    blueBlock.appendChild(degresymbol)
+
+    drag.appendChild(blueBlock)
+
+    // red content
+
+    const redBlock = document.createElement('div')
+    redBlock.classList.add('block', 'brown')
+
+    var degre = document.createTextNode(`Para`);
+    redBlock.appendChild(degre)
+
+    const speedInput = document.createElement('input')
+    speedInput.setAttribute('type', 'number')
+    speedInput.setAttribute('placeholder', 'velo...')
+    redBlock.appendChild(speedInput)
+
+    var degresymbol = document.createTextNode(`°`);
+    redBlock.appendChild(degresymbol)
+
+    drag.appendChild(redBlock)
+
+    newBoard.appendChild(idt)
+    newBoard.appendChild(motorInput)
+    newBoard.appendChild(drag)
+    hereBlocks.appendChild(newBoard)
+})
+
+const enviar = document.getElementById('enviar')
+
+enviar.addEventListener('click', () => {
+    var i = 1;
     const motorinput = document.querySelectorAll('#board .yellow input')
-    var i = 0;
+
     motorinput.forEach(motor => {
         i += 1
-        motor.setAttribute('id', `motor-${i}`)
+        motor.setAttribute('name', `motor-${i}`)
         console.log(i)
     })
 })
