9d5d29fb767a0a78831300898c2c52f43049f732
kluiwert <kluiwertvictor@super.ufa.edu.br>
2021-03-16 10:33:05 -0400

    update trail 22 and 15

--- website-armblock/src/server.js
+++ website-armblock/src/server.js
@@ -131,9 +131,6 @@ server.get("/trail-content-24", (req, res) => {
 server.get("/trail-content-25", (req, res) => {
     res.sendFile(__dirname + "/views/trail-content-25.html")
 })
-server.get("/trail-content-26", (req, res) => {
-    res.sendFile(__dirname + "/views/trail-content-26.html")
-})
 server.get("/tutorial", (req, res) => {
     res.sendFile(__dirname + "/views/video.html")
 })
